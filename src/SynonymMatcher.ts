73d87c57d3d7dab109409ed95b810169cade82bf
Oliver Ross <oliver.ross@hotmail.com>
2021-07-14 23:02:33 +0100

    iterate matchAll iterator instead of reduce inMem array

--- src/SynonymMatcher.ts
+++ src/SynonymMatcher.ts
@@ -24,34 +24,23 @@ export class SynonymMatcher {
 
   private * search (string: string): IterableIterator<Match[]> {
     for (const key of this.registry.keys()) {
-      const matches = Array.from(
-        string.matchAll(this.getSearchExpression(key))
-      )
-      yield matches.reduce(
-        this.getMatchReducer(key),
-        []
-      )
+      const expression = this.getSearchExpression(key)
+      const output = []
+      for (const match of string.matchAll(expression)) {
+        if (typeof (match.index) !== 'undefined') {
+          output.push({
+            match: key,
+            location: match.index,
+            length: key.length,
+            synonyms: this.registry.getSynonymous(key)
+          })
+        }
+      }
+      yield output
     }
   }
 
   private getSearchExpression (string: string): RegExp {
     return new RegExp(`\\b(${string})\\b`, 'gm')
-  }
-
-  private getMatchReducer (key: string): (
-    output: Match[],
-    match: RegExpMatchArray
-  ) => Match[] {
-    return (output, match) => {
-      if (typeof (match.index) !== 'undefined') {
-        output.push({
-          match: key,
-          location: match.index,
-          length: key.length,
-          synonyms: this.registry.getSynonymous(key)
-        })
-      }
-      return output
-    }
   }
 }
